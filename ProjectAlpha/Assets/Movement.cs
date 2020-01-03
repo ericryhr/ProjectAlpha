bed0f733491667181a7af46f7240757634e77780
Eric Ryhr <59489835+ericryhr@users.noreply.github.com>
2020-01-03 23:59:57 +0100

    Changes1

---
+++ ProjectAlpha/Assets/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 20f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Hello World");
+        }
+    }
+}
